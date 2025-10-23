4ff53807fdb719115cd8a4f1ca7510e6a43dc481
11lcb <13525090273@163.com>
2025-10-23 19:46:54 +0300

    2025 10.23

---
+++ books-en.csv .py
@@ -0,0 +1,21 @@
+import csv
+with open ('books-en.csv','r',encoding='cp1251') as file:
+    csv_reader = csv.reader(file, delimiter=';',quotechar='"')
+    print(f" {'\x1b[48;5;88m'} use 'quorechar' for skipping delimiter in the asked_row {'\x1b[0m'}")
+    first_sentence = next (csv_reader)
+    
+    limitted_year = []
+    asked_year='1990'
+    for row in csv_reader:
+        time=row[3]
+        if int(time) <=  int(asked_year):
+            limitted_year.append(row)
+         
+    print(f"books_year to {int(asked_year)} , have {len(limitted_year)} books")        
+
+        
+
+
+
+
+
--- csv文件，筛选年份 .py
+++ csv文件，筛选年份 .py
@@ -1,16 +1,5 @@
 import csv
 
-   
-def serch_accurate_years_book():
-    asked_time = ["2014","2016","2017"]
-    time_row = []
-    for row in csv_reader:
-        time = row[6]
-        day,month,year = time.split('.')
-        year_1,when_1 = year.split()
-        if year_1 in asked_time:
-            time_row.append(row)
-    print(f'asked_time: {asked_time}.  have {len(time_row)} books')
 
 def serch_area_years_books(csv_reader):
     a= input("limitted year: ")
@@ -31,13 +20,4 @@ with open ('books.csv','r',encoding="cp1251") as file:
     #serch_accurate_years_book()
     print()
     serch_area_years_books(list(csv_reader))
-
-            
-
-
-
-
-            
-          
-
 
