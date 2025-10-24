d526886ffe177e803c95275a9e4c97db0fcdb721
11lcb <13525090273@163.com>
2025-10-24 19:31:12 +0300

    10.24.2025

---
+++ 10.21.2025 csv文件。筛选年份2.py
@@ -0,0 +1,18 @@
+import csv
+
+def serch_accurate_years_book():
+    asked_time = ["2014","2016","2017"]
+    time_row = []
+    for row in csv_reader:
+        time = row[6]
+        day,month,year = time.split('.')
+        year_1,when_1 = year.split()
+        if year_1 in asked_time:
+            time_row.append(row)
+    print(f'asked_time: {asked_time}.  have {len(time_row)} books')
+
+with open ('books.csv','r',encoding="cp1251") as file:
+    csv_reader = csv.reader(file,delimiter=';')
+
+    firse_one = next(csv_reader)
+    serch_accurate_years_book()
